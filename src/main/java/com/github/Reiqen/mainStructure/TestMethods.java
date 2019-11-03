4fcebc2e7a6278c7ddedf413fb4e91ca9978fa58
Roman Kuznetcov <reiqen@yahoo.com>
2019-11-03 20:40:25 +0400

    Construct new project

---
+++ src/main/java/com/github/Reiqen/mainStructure/TestMethods.java
@@ -0,0 +1,19 @@
+package com.github.Reiqen.mainStructure;
+
+/** This class is made to check if the methods
+ *  from below are working.
+ * @author Roman Kuznetcov
+ *
+ */
+public class TestMethods {
+
+	public String getAuthorsName() {
+		return "Roman Kuznetcov";
+	}
+	
+	public void printAuthorsName() {
+		System.out.println("Roman Kuznetcov");
+	}
+	
+	
+}
